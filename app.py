a30bd0dbb8ffa0c50e1215e7633f08a47c06daf5
tulasi31 <tulasi310322@gmail.com>
2025-02-12 14:36:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,5 +52,9 @@ def display_data():
     return result
 
 
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT dynamically
+    app.run(host='0.0.0.0', port=port)
+
